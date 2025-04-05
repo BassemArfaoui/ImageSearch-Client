1158ca2b8ef71dae9b31fd491b3a0baacf9f9c42
BassemArfaoui <bassem.arfaoui@insat.ucar.tn>
2025-04-05 16:38:59 +0100

    add project layout

---
+++ src/app/App.jsx
@@ -0,0 +1,43 @@
+import { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import Main from "../components/layouts/Main";
+import Test from "../components/test/Test";
+import "./App.css";
+
+const queryClient = new QueryClient();
+
+// Lazy-loaded pages
+const HomePage = lazy(() => import("../pages/HomePage"));
+const FavouritePage = lazy(() => import("../pages/FavouritePage"));
+const SettingsPage = lazy(() => import("../pages/SettingsPage"));
+const SearchPage = lazy(() => import("../pages/SearchPage"));
+
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="text-3xl font-bold">
+        <Router>
+          <Main>
+            <Suspense fallback={<div className="text-center">Loading...</div>}>
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/favourite" element={<FavouritePage />} />
+                <Route path="/search" element={<SearchPage />} />
+                <Route path="/settings" element={<SettingsPage />} />
+                <Route path="/test" element={<Test />} />
+              </Routes>
+            </Suspense>
+          </Main>
+        </Router>
+      </div>
+
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
